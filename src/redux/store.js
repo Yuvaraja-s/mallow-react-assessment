360c9c5cef20d27953c6a2d29769c061dd1fb55d
Yuvaraja <yuvarajaselvapandi5@gmail.com>
2025-09-27 16:05:48 +0530

    Fix: removed redux-devtools-extension for Netlify build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from './reducers/userReducer';
 
 const rootReducer = combineReducers({
-  users: userReducer
+  users: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
