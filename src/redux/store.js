f297134fb237b79bcd73c720695224f9f4255800
Yuvaraja <yuvarajaselvapandi5@gmail.com>
2025-09-27 13:16:11 +0530

    Setup Redux store and provider

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './reducers/userReducer';
+
+const rootReducer = combineReducers({
+  users: userReducer
+});
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
