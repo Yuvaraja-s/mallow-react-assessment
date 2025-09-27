a0399bf873c0b2910ac0fb64f077502390553a45
Yuvaraja <yuvarajaselvapandi5@gmail.com>
2025-09-27 13:20:51 +0530

    Created reusable Modal component

---
+++ src/components/modals/Modal.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContent = styled.div`
+  background: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  width: 400px;
+  max-width: 90%;
+`;
+
+const CloseButton = styled.button`
+  background: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  float: right;
+  cursor: pointer;
+`;
+
+export default function Modal({ children, onClose }) {
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <CloseButton onClick={onClose}>X</CloseButton>
+        {children}
+      </ModalContent>
+    </ModalWrapper>
+  );
+}
